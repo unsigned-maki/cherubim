0c88d64a3bfafef91977e87c917c772d2cb26a81
Niclas Nell <nel20326@spengergasse.at>
2021-12-05 22:52:26 +0100

    style(lexer): format code

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -11,93 +11,91 @@ using namespace std;
 namespace cherubim::lexer
 {
 
-CTokenTable g_tokenTable;
+    CTokenTable g_tokenTable;
 
-inline char up(char c)
-{
-    return (c > 0x60 && c < 0x7B) ? c ^ 0x20 : c;
-}
-
-void postProcess(string *raw)
-{
-    string tmp;
- 
-    for (string::iterator i = raw->begin(); i < raw->end(); i++)
+    inline char up(char c)
     {
-        if (g_tokenTable.getSynTokens()->find(*i) != g_tokenTable.getSynTokens()->end())
-        {
-            tmp.push_back(' ');
-            tmp.push_back(*i);
-            tmp.push_back(' ');
-        }
-        else if (g_tokenTable.getComplexTokens()->find(make_pair(*i, *(i+1))) != g_tokenTable.getComplexTokens()->end())
-        {
-            tmp.push_back(' ');
-            tmp.push_back(*i);
-            tmp.push_back(*(i+1));
-            tmp.push_back(' ');
-            *i++;
-        }
-        else
-            tmp.push_back(*i);
+        return (c > 0x60 && c < 0x7B) ? c ^ 0x20 : c;
     }
 
-    *raw = tmp;
-}
-
-vector<string> v_splitAndClean(string input)
-{
-    vector<string> splitted;
-    string tmp;
-
-    for (string::iterator i = input.begin(); i < input.end(); i++)
+    void postProcess(string *raw)
     {
-        if (*i == ' ' || *i == '\n' || *i == '\t' || *i == '\b' || *i == '\r')
-        {
-            splitted.push_back(tmp);
-            tmp.clear();
-            continue;
-        }
-
-        tmp.push_back(up(*i));
-
-        if (!splitted.back().length())
-            splitted.pop_back();
-    }
+        string tmp;
     
-    return splitted;
-}
-
-vector<SToken> lex(string input)
-{
-    vector<SToken> tokens;
-    vector<string> splitInput;
-
-    g_tokenTable = CTokenTable();
-
-    postProcess(&input);
-    
-    cout << input << endl;
-
-    splitInput = v_splitAndClean(input);
-
-    for (vector<string>::iterator i = splitInput.begin(); i < splitInput.end(); i++)
-    {
-        map<string, EToken>::iterator lookup = g_tokenTable.find(*i);
-
-        if (lookup != g_tokenTable.end())
+        for (string::iterator i = raw->begin(); i < raw->end(); i++)
         {
-            SToken token = {lookup->first, lookup->second};
-            tokens.push_back(token);
+            if (g_tokenTable.getSynTokens()->find(*i) != g_tokenTable.getSynTokens()->end())
+            {
+                tmp.push_back(' ');
+                tmp.push_back(*i);
+                tmp.push_back(' ');
+            }
+            else if (g_tokenTable.getComplexTokens()->find(make_pair(*i, *(i+1))) != g_tokenTable.getComplexTokens()->end())
+            {
+                tmp.push_back(' ');
+                tmp.push_back(*i);
+                tmp.push_back(*(i+1));
+                tmp.push_back(' ');
+                *i++;
+            }
+            else
+                tmp.push_back(*i);
         }
-        else
-        {
-            SToken token = {*i, ID_IDENTIFIER};
-            tokens.push_back(token);
-        }
+
+        *raw = tmp;
     }
 
-    return tokens;
-}
+    vector<string> splitAndClean(string input)
+    {
+        vector<string> splitted;
+        string tmp;
+
+        for (string::iterator i = input.begin(); i < input.end(); i++)
+        {
+            if (*i == ' ' || *i == '\n' || *i == '\t' || *i == '\b' || *i == '\r')
+            {
+                splitted.push_back(tmp);
+                tmp.clear();
+                continue;
+            }
+
+            tmp.push_back(up(*i));
+
+            if (!splitted.back().length())
+                splitted.pop_back();
+        }
+        
+        return splitted;
+    }
+
+    vector<SToken> lex(string input)
+    {
+        vector<SToken> tokens;
+        vector<string> splitInput;
+
+        g_tokenTable = CTokenTable();
+
+        postProcess(&input);
+        
+        splitInput = splitAndClean(input);
+
+        for (vector<string>::iterator i = splitInput.begin(); i < splitInput.end(); i++)
+        {
+            map<string, EToken>::iterator lookup = g_tokenTable.find(*i);
+
+            if (lookup != g_tokenTable.end())
+            {
+                SToken token = {lookup->first, lookup->second};
+                tokens.push_back(token);
+            }
+            else
+            {
+                SToken token = {*i, ID_IDENTIFIER};
+                tokens.push_back(token);
+            }
+        }
+
+        return tokens;
+    }
 
 }
--- src/lexer/token.cpp
+++ src/lexer/token.cpp
@@ -13,60 +13,60 @@ using namespace std;
 namespace cherubim::lexer
 {
 
-CTokenTable::CTokenTable()
-{    
-    addComplexToken(':', '=', OP_DEFINE);
+    CTokenTable::CTokenTable()
+    {    
+        addComplexToken(':', '=', OP_DEFINE);
 
-    addSynToken('(', SYN_BRACE_OPEN);
-    addSynToken('[', SYN_BRACE_OPEN);
-    addSynToken('{', SYN_BRACE_OPEN);
-    addSynToken(')', SYN_BRACE_CLOSE);
-    addSynToken(']', SYN_BRACE_CLOSE);
-    addSynToken('}', SYN_BRACE_CLOSE);
-    addSynToken(';', SYN_SEPERATOR);
+        addSynToken('(', SYN_BRACE_OPEN);
+        addSynToken('[', SYN_BRACE_OPEN);
+        addSynToken('{', SYN_BRACE_OPEN);
+        addSynToken(')', SYN_BRACE_CLOSE);
+        addSynToken(']', SYN_BRACE_CLOSE);
+        addSynToken('}', SYN_BRACE_CLOSE);
+        addSynToken(';', SYN_SEPERATOR);
 
-    insert(make_pair("IF", OP_IF));
-    insert(make_pair("IFF", OP_IFF));
-    insert(make_pair("NOT", OP_NOT));
-    insert(make_pair("AND", OP_AND));
-    insert(make_pair("NAND", OP_NAND));
-    insert(make_pair("OR", OP_OR));
-    insert(make_pair("XOR", OP_XOR));
-    insert(make_pair("NOR", OP_NOR));
-    insert(make_pair("XNOR", OP_XNOR));
-    insert(make_pair("TRUE", LTRL_TRUE));
-    insert(make_pair("FALSE", LTRL_FALSE));
-    insert(make_pair("T", LTRL_TRUE));
-    insert(make_pair("F", LTRL_FALSE));
-    insert(make_pair("1", LTRL_TRUE));
-    insert(make_pair("0", LTRL_FALSE));
-}
+        insert(make_pair("IF", OP_IF));
+        insert(make_pair("IFF", OP_IFF));
+        insert(make_pair("NOT", OP_NOT));
+        insert(make_pair("AND", OP_AND));
+        insert(make_pair("NAND", OP_NAND));
+        insert(make_pair("OR", OP_OR));
+        insert(make_pair("XOR", OP_XOR));
+        insert(make_pair("NOR", OP_NOR));
+        insert(make_pair("XNOR", OP_XNOR));
+        insert(make_pair("TRUE", LTRL_TRUE));
+        insert(make_pair("FALSE", LTRL_FALSE));
+        insert(make_pair("T", LTRL_TRUE));
+        insert(make_pair("F", LTRL_FALSE));
+        insert(make_pair("1", LTRL_TRUE));
+        insert(make_pair("0", LTRL_FALSE));
+    }
 
-bool CTokenTable::addSynToken(char token, EToken type)
-{
-    m_syntacticTokens.insert(token);
-    return insert(make_pair(string(1, token), type)).second;
-}
+    bool CTokenTable::addSynToken(char token, EToken type)
+    {
+        m_syntacticTokens.insert(token);
+        return insert(make_pair(string(1, token), type)).second;
+    }
 
-bool CTokenTable::addComplexToken(char first, char second, EToken type)
-{
-    m_complexTokens.insert(make_pair(first, second));
-    return insert(make_pair(string(1, first) + string(1, second), type)).second;
-}
+    bool CTokenTable::addComplexToken(char first, char second, EToken type)
+    {
+        m_complexTokens.insert(make_pair(first, second));
+        return insert(make_pair(string(1, first) + string(1, second), type)).second;
+    }
 
-bool CTokenTable::addCall(string call)
-{
-    return insert(make_pair(call, KWD_CALL)).second;
-}
+    bool CTokenTable::addCall(string call)
+    {
+        return insert(make_pair(call, KWD_CALL)).second;
+    }
 
-set< pair<char, char> > *CTokenTable::getComplexTokens()
-{
-    return &m_complexTokens;
-}
+    set< pair<char, char> > *CTokenTable::getComplexTokens()
+    {
+        return &m_complexTokens;
+    }
 
-set<char> *CTokenTable::getSynTokens()
-{
-    return &m_syntacticTokens;
-}
+    set<char> *CTokenTable::getSynTokens()
+    {
+        return &m_syntacticTokens;
+    }
 
 }
