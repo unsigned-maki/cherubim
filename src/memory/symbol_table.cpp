65619846f87c17c54cea910c23a3ea383ff0763a
Niclas <NEL20326@spengergasse.at>
2021-12-06 14:04:15 +0100

    docs(symbol_table): update CSymbolTable::read documentation

--- src/memory/symbol_table.cpp
+++ src/memory/symbol_table.cpp
@@ -12,7 +12,7 @@ namespace cherubim::memory
     /**
      * @brief looks up a given symbol name and writes an gives a pointer to it.
      * 
-     * @param symbol pointer to a pointer to a CSymbol class, which the pointer to the found symbol will be written to.  
+     * @param symbol pointer to a pointer to a CSymbol class instance, which the pointer to the found symbol will be written to.  
      * @param name symbol name to look up.
      * @return ESymbol type of the symbol that was read, returns SYM_NIL if it's an empty symbol or no symbol was found. 
      */
