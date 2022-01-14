1b25feee77a800ad1121379d3588fcb2ea664cb3
Niclas Nell <nel20326@spengergasse.at>
2022-01-14 19:55:33 +0100

    feat(parser): add error

---
+++ src/parser/error.h
@@ -0,0 +1,29 @@
+//
+// Created by Niclas on 10.01.2022.
+//
+
+#ifndef POS1_ERROR_H
+#define POS1_ERROR_H
+#include <string>
+
+using namespace std;
+
+enum EError
+{
+    ERR_SYNTAX,
+    ERR_NAME
+};
+
+struct SError {
+    SError(EError type, string message, long at)
+    {
+        this->m_type = type;
+        this->m_message = message;
+        this->at = at;
+    };
+    EError m_type;
+    string m_message;
+    unsigned long at;
+};
+
+#endif //POS1_ERROR_H
