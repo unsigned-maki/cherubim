9caebed4a01bb32475a198f448e794b76b0608f1
Niclas Nell <nel20326@spengergasse.at>
2022-01-20 22:18:47 +0100

    feat(error): add ERR_FROM_TO error type

--- lib/parser/error.hpp
+++ lib/parser/error.hpp
@@ -11,15 +11,21 @@ using namespace std;
 enum EError
 {
     ERR_SYNTAX,
-    ERR_NAME
+    ERR_NAME,
+    ERR_FROM_TO
 };
 
 struct SError {
+    SError()
+    {
+        this->m_hit = false;
+    };
     SError(EError type, string message, long at)
     {
         this->m_type = type;
         this->m_message = message;
         this->m_at = at;
+        this->m_hit = true;
     };
     EError m_type;
     string m_message;
